ff7812b0ea057fc92548ca10be7083ae64cc60bf
Gianni Bischoff <Oucori@gmail.com>
2023-04-03 12:49:17 +0200

    minor text change on "/" page

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ const app = express();
 export const add = (a: number, b: number): number => a + b;
 
 app.get('/', (_req, res) => {
-  res.send('Hello World!');
+  res.send('Hello Gianni !');
 });
 
 export const server = app.listen(3000, () => {
